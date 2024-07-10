2b1168b52b5211a3f8443a9476c5fc77f10a91dc
rohanzemoso <rohan.jadhao@zemosolabs.com>
2024-07-10 14:13:42 +0530

    :sparkles: Add React and Javascript Demo Files

---
+++ Demo/Question2.js
@@ -0,0 +1,20 @@
+const users = [
+    { user: 'user1', designation: 'IT' },
+    { user: 'user2', designation: 'CSE' },
+  ];
+  
+  const groupUsers = (usersArray) => {
+    return usersArray.reduce((group, user) => {
+      const { designation } = user;
+      if (!group[designation]) {
+        group[designation] = [];
+      }
+      group[designation].push(user);
+      return group;
+    }, {});
+  };
+  
+  const group = groupUsers(users);
+  
+  console.log(group);
+  

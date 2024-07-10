8e82b7fd4465f57868678b3024da7b10745361b3
rohanzemoso <rohan.jadhao@zemosolabs.com>
2024-07-10 20:31:26 +0530

    edit to question2

--- Demo/Question2.js
+++ Demo/Question2.js
@@ -1,20 +1,17 @@
 const users = [
-    { user: 'user1', designation: 'IT' },
-    { user: 'user2', designation: 'CSE' },
-  ];
-  
-  const groupUsers = (usersArray) => {
-    return usersArray.reduce((group, user) => {
-      const { designation } = user;
-      if (!group[designation]) {
-        group[designation] = [];
-      }
-      group[designation].push(user);
-      return group;
-    }, {});
-  };
-  
-  const group = groupUsers(users);
-  
-  console.log(group);
-  
+  { user: 'user1', designation: 'IT' },
+  { user: 'user2', designation: 'CSE' },
+];
+
+const groupUsers = (usersArray) => {
+  return usersArray.reduce((group, user) => {
+    const { designation } = user;
+    group[designation] ??= [];
+    group[designation].push(user);
+    return group;
+  }, {});
+};
+
+const group = groupUsers(users);
+
+console.log(group);
